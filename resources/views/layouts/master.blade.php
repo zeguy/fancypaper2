d4770f7e4a3581493d85954681f72b05935f862f
zeguy <smullan.csheep@gmail.com>
2017-12-13 18:12:28 -0500

    fixing w3s validaiton

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,72 +2,72 @@
 <html lang="en">
 <head>
 
-	<title>Fancypaper</title>
+    <title>Fancypaper</title>
   
-	<meta charset="utf-8">
+    <meta charset="utf-8">
   
-	<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
-	<link rel="stylesheet" href="/css/fancypaper2.css">
+    <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="/css/fancypaper2.css">
 
 </head>
 <body>
 
-	<header>
-		@php
-			# Define a PHP array of links and their labels
-			# Quite a bit of straight PHP code here, but arguably ok
-			# because it's display specific and allows you to edit the link
-			# labels without having to edit a logic file.
-			if(Auth::check()) {
-				$nav = [
-					'posters/breakeven' => 'Breakeven',
-					'posters/index' => 'Prints',
-					'posters/create' => 'Add Print',
-					'posters/sold' => 'Sales',
-				];
-			} else {
-				$nav = [
-					'register' => 'Register',
-					'login' => 'Login',
-					'posters/breakeven' => 'Breakeven',
-				];
-			}
-		@endphp
+    <header>
+        @php
+            # Define a PHP array of links and their labels
+            # Quite a bit of straight PHP code here, but arguably ok
+            # because it's display specific and allows you to edit the link
+            # labels without having to edit a logic file.
+            if(Auth::check()) {
+                $nav = [
+                    'posters/breakeven' => 'Breakeven',
+                    'posters/index' => 'Prints',
+                    'posters/create' => 'Add Print',
+                    'posters/sold' => 'Sales',
+                ];
+            } else {
+                $nav = [
+                    'register' => 'Register',
+                    'login' => 'Login',
+                    'posters/breakeven' => 'Breakeven',
+                ];
+            }
+        @endphp
 
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-			    <div class="navbar-header">
-           			<a class="navbar-brand" href="/">FancyPaper</a>
-         		</div>
-				<ul class="nav navbar-nav">
-					@foreach($nav as $link => $label)
-						<li><a href='/{{ $link }}' class='{{ Request::is($link) ? 'active' : '' }}'>{{ $label }}</a>
-					@endforeach
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                       <a class="navbar-brand" href="/">FancyPaper</a>
+                 </div>
+                <ul class="nav navbar-nav">
+                    @foreach($nav as $link => $label)
+                        <li><a href='/{{ $link }}' class='{{ Request::is($link) ? 'active' : '' }}'>{{ $label }}</a>
+                    @endforeach
 
-					@if(Auth::check())
-						<li>
-							<form method='POST' id='logout' action='/logout'>
-								{{csrf_field()}}
-								<a href='#' onClick='document.getElementById("logout").submit();'>Logout {{ $user->name }}</a>
-							</form>
-						</li>
-					@endif
-				</ul>
-			</div>
-		</nav>
-  	</header>
+                    @if(Auth::check())
+                        <li>
+                            <form method='POST' id='logout' action='/logout'>
+                                {{csrf_field()}}
+                                <a href='#' onClick='document.getElementById("logout").submit();'>Logout {{ $user->name }}</a>
+                            </form>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </nav>
+      </header>
 
-	<section>
-    	@yield('content')
-	</section>
+    <section>
+        @yield('content')
+    </section>
 
-	<footer class="container-fluid text-center">
-  		<div class="footer-copyright">
-        	<div class="container-fluid">
-            	© 2017 Copyright: <a href="https://p1.zeguy.me"> ZeGuy </a>
-        	</div>
-    	</div>
-	</footer>
+    <footer class="container-fluid text-center">
+          <div class="footer-copyright">
+            <div class="container-fluid">
+                © 2017 Copyright: <a href="https://p1.zeguy.me"> ZeGuy </a>
+            </div>
+        </div>
+    </footer>
 
 </body> 
 </html>
--- resources/views/posters/breakeven.blade.php
+++ resources/views/posters/breakeven.blade.php
@@ -3,43 +3,43 @@
 @section('content')
 
 <form method='POST' action='/posters/breakeven'>
-	{{ csrf_field() }}
-	<fieldset name="breakeven">
+    {{ csrf_field() }}
+    <fieldset name="breakeven">
 
-		<legend><b>Breakeven Calculator:</b></legend>
+        <legend><b>Breakeven Calculator:</b></legend>
 
         <div class="form-group">
             <label for="cost">Print Cost:</label>
-            <input type="number" "form-control" name="cost" id="cost" required="required" placeholder="required" min="0" step="0.01" value=''>
+            <input type="number" class="form-control" name="cost" id="cost" required="required" placeholder="required" min="0" step="0.01" value=''>
         </div>
 
-		<label for="platform">Platform:</label>
-			<select name="platform" "form-control"  id="platform">
-				<option value="ebay">ebay</option>
-				<option value="expresso">expresso</option>
-				<option value="shopify">shopify</option>
-			</select>
+        <label for="platform">Platform:</label>
+            <select name="platform" class="form-control"  id="platform">
+                <option value="ebay">ebay</option>
+                <option value="expresso">expresso</option>
+                <option value="shopify">shopify</option>
+            </select>
 
-		<div class="checkbox">
+        <div class="checkbox">
             <label>
             <input type="checkbox" name="shipping" id="shipping">Free Shipping:
             </label>
         </div>
 
-		<button type="submit" name="submit" value="Submit">Submit</button>
+        <button type="submit" name="submit" value="Submit">Submit</button>
 
-	</fieldset>
+    </fieldset>
 </form>
 
 <h6>Results:</h6>
 @if(count($errors) > 0)
-	<ul>
-		@foreach ($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-	</ul>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
 @else
-	{{ $message }}
+    {{ $message }}
 @endif
 
 
