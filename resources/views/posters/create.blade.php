80997910921f7ebed32020ae1d195dfeb96bf1bd
zeguy <smullan.csheep@gmail.com>
2017-12-13 18:18:59 -0500

    fixing w3s validaiton

--- resources/views/posters/create.blade.php
+++ resources/views/posters/create.blade.php
@@ -33,7 +33,7 @@
 
         <div class="form-group">
             <label for='image'>* Image URL </label>
-            <input type='text' class="form-control" max='4' name='image' id='image' value='{{ old('image') }}'>
+            <input type='text' class="form-control" name='image' id='image' value='{{ old('image') }}'>
             @include('modules.error-field', ['fieldName' => 'image'])
         </div>
 
